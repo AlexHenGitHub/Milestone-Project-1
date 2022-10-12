8a262d54a77bcc2b3c7368225c6424a3f414932a
AlexHenGitHub <108377905+AlexHenGitHub@users.noreply.github.com>
2022-10-11 23:49:31 -0400

    reworked response function

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 
 
-function RandomInt(max) 
+//const UserInput = prompt("0-2")
+
+
+
+async function RandomInt() 
 {
-    let RanNum = Math.floor(Math.random() * max)
+
+    let RanNum = Math.floor(Math.random() * 3)
     let RanOuput = ["Rock", "Paper", "Scissors"];
+
     console.log(RanOuput[RanNum]);
 
     return ;
   }
+RandomInt()
   
+//   if(UserInput == "0")
+//   {
+      
+//   }
+//   if(UserInput == "1")
+//   {
+      
+//   }
+//   else() =>
+//   {
+      
+//   }
 
-RandomInt(3)
   
 
   
