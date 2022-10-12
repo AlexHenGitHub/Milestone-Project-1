89d43d3434aec645d9337754c965b875370ccaf8
AlexHenGitHub <108377905+AlexHenGitHub@users.noreply.github.com>
2022-10-11 22:56:42 -0400

    Basic output generator for the ai to respone to the player with

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function RandomInt(max) 
+{
+    let RanNum = Math.floor(Math.random() * max)
+    let RanOuput = ["Rock", "Paper", "Scissors"];
+    console.log(RanOuput[RanNum]);
+
+    return ;
+  }
+  
+
+RandomInt(3)
+  
+
+  
+ 
+  
