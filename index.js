3f367ce3e15426f06b5ebc2689115badea44edb2
AlexHenGitHub <108377905+AlexHenGitHub@users.noreply.github.com>
2022-10-12 00:37:43 -0400

    Adding onto the computer response function

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 
 
-//const UserInput = prompt("0-2")
+//const UserInput = prompt("Rock,Paper, Scissors!")
 
+let UserInput = 1
 
-
-async function RandomInt() 
+async function RandomInt(UserInput) 
 {
 
     let RanNum = Math.floor(Math.random() * 3)
     let RanOuput = ["Rock", "Paper", "Scissors"];
+    let AiOutput = RanOuput[RanNum];
 
-    console.log(RanOuput[RanNum]);
+    if(UserInput = "Rock" && AiOutput == RanOuput[2])
+    {
+        console.log("You win");
+    }
+    if(UserInput = "Paper" && AiOutput == RanOuput[0])
+    {
+        console.log("You win");
+    }
+    if(UserInput = "Scissors" && AiOutput == RanOuput[1])
+    {
+        console.log("You win");
+    }
+    // else() =>
+    // {
+        
+    // }
+  
+    //console.log(RanOuput[RanNum]);
 
     return ;
   }
-RandomInt()
+RandomInt(UserInput)
   
-//   if(UserInput == "0")
-//   {
-      
-//   }
-//   if(UserInput == "1")
-//   {
-      
-//   }
-//   else() =>
-//   {
-      
-//   }
+
 
   
 
