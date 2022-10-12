dca9a96e576583318985ce87615a11b42b86f29e
AlexHenGitHub <108377905+AlexHenGitHub@users.noreply.github.com>
2022-10-12 12:20:20 -0400

    Connecting the buttons to the game function

--- index.js
+++ index.js
@@ -1,28 +1,77 @@
-
+let wins = 0
+let loses = 0
+let count = 0
 
 //const UserInput = prompt("Rock,Paper, Scissors!")
 
-let UserInput = 1
 
-async function RandomInt(UserInput) 
+function OnClick()
 {
 
+    console.log("Test")
+
+}
+
+
+//let UserInput = 1
+
+async function RandomInt(UserInput) 
+{   
+
+    // let winStat = console.log("You win")
+    // let losStat = console.log("You lose")
+    // let tie = console.log("Tie")
+    
     let RanNum = Math.floor(Math.random() * 3)
     let RanOuput = ["Rock", "Paper", "Scissors"];
     let AiOutput = RanOuput[RanNum];
+    console.log(AiOutput)
 
-    if(UserInput = "Rock" && AiOutput == RanOuput[2])
+    //Rock outputs
+    if(UserInput == "Rock" && AiOutput == "Scissors")//Roc v Sci
     {
-        console.log("You win");
+        console.log("You win")
+        wins++
     }
-    if(UserInput = "Paper" && AiOutput == RanOuput[0])
+    else if(UserInput == "Rock" && AiOutput == "Paper" )//Roc v Pap
     {
-        console.log("You win");
+        console.log("You lose")
+        loses++
     }
-    if(UserInput = "Scissors" && AiOutput == RanOuput[1])
+    
+    //Paper Outputs
+    if(UserInput == "Paper" && AiOutput == RanOuput[0])//Pap v Roc
     {
-        console.log("You win");
+        
+        wins++
     }
+    if(UserInput == "Rock" && AiOutput == RanOuput[2])//Pap v SCi
+    {
+        
+        loses++
+    }
+
+    //Scissors Outputs
+
+    if(UserInput == "Scissors" && AiOutput == RanOuput[1])//Sci v Pap
+    {
+        
+        wins++
+    }
+    if(UserInput == "Scissors" && AiOutput == RanOuput[0])//Sci v Roc
+    {
+        
+        loses++
+    }
+     
+    //Tie
+    if(UserInput == AiOutput)//Roc v Roc
+    {
+        console.log("Tie")
+    
+    }
+    
+//alert("Tie"); 
     // else() =>
     // {
         
@@ -30,10 +79,12 @@ async function RandomInt(UserInput)
   
     //console.log(RanOuput[RanNum]);
 
-    return ;
-  }
-RandomInt(UserInput)
+
+}
+
+
   
+
 
 
   
